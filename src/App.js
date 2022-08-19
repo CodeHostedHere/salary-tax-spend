0960b8e8f35a66b515c095de832283d4151cbc6c
glacey <garretrlacey@gmail.com>
2022-08-19 11:30:14 +0100

    Separate in to component files

--- src/App.js
+++ src/App.js
@@ -2,15 +2,16 @@
 import React from 'react';
 import axios from 'axios';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import ToggleButton from 'react-bootstrap/ToggleButton';
-import ButtonGroup from 'react-bootstrap/ButtonGroup';
-import Dropdown from 'react-bootstrap/Dropdown';
-import Table from 'react-bootstrap/Table';
 import Alert from 'react-bootstrap/Alert';
-import Placeholder from 'react-bootstrap/Placeholder';
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
+
+import SalaryTable from './components/SalaryTable';
+import TripleButtonGroup from './components/TripleButtonGroup';
+import TripleButtonPlaceholder from './components/TripleButtonPlaceholder';
+import DropDown from './components/DropDown';
+import SpendTable from './components/SpendTable';
 
 const API_ENDPOINT = 'http://localhost:8080/';
 const CITIES_URL = `${API_ENDPOINT}city/all`;
@@ -137,214 +138,6 @@ function App() {
     </Container>
   );
 };
-
-const SpendTable = ({ salariesGiven, radioValue, taxGiven, spend }) => {
-  if (taxGiven.data.length === 0) {
-    return <></>;
-  }
-
-  let salaryCompensationonverted = salariesGiven.data[radioValue-1].compensationConverted;
-
-  let taxPayableConverted = taxGiven.data.taxPayableConverted;
-  let stringFormatOptions = { 
-    minimumFractionDigits: 2,
-    maximumFractionDigits: 2 
-  };
-  let netMonthlyIncome = ((salaryCompensationonverted - taxPayableConverted) / 12);
-  let formattedNetMonthlyIncome = netMonthlyIncome.toLocaleString('en', stringFormatOptions);
-  let totalSpend = 0.0;
-  spend.data.map(spendInfo => {
-    totalSpend += spendInfo[1] * spendInfo[2];
-  });
-  let formattedTotalSpend = totalSpend.toLocaleString('en', stringFormatOptions);
-  let monthlySavings = netMonthlyIncome - totalSpend;
-  let formattedMonthlySavings = monthlySavings.toLocaleString('en', stringFormatOptions);
-  return (
-    <Table bordered striped size="sm" variant="dark">
-      <tbody>
-      <tr>
-        <th>Net Monthly Income </th>
-        <td></td><td></td><td></td>
-        <th className="align-right">€{formattedNetMonthlyIncome}</th>
-      </tr>
-        <tr>
-          <th>Expenses</th>
-          <th>Cost</th>
-          <th>Amount</th>
-          <th>Total</th>
-          <td></td>
-        </tr>
-        { spend.data.map(spendInfo => (
-          <tr>
-            <td>{spendInfo[0]}</td>
-            <td>€ {spendInfo[1].toLocaleString('en', stringFormatOptions)}</td>
-            <td>{spendInfo[2]}</td>
-            <td> € {(spendInfo[1] * spendInfo[2]).toLocaleString('en', stringFormatOptions)}</td>
-            <td></td>
-          </tr>
-        ))}
-        <tr>
-          <th>Expenses Total</th>
-          <td></td><td></td>
-          <td></td>
-          <th className="align-right">€ {formattedTotalSpend}</th>
-        </tr>
-        <tr>
-          <th>Monthly Savings</th>
-          <td></td><td></td>
-          <td></td>
-          <th className="align-right">€ {formattedMonthlySavings}</th>
-        </tr>
-      </tbody>
-    </Table>
-  );
-}
-
-
-const DropDown = ({ title, setTitleFunction, cities }) => {
-  return (
-    <Dropdown onSelect={setTitleFunction}>
-      <Dropdown.Toggle style={{ width: '100%'}}>
-        {title}
-      </Dropdown.Toggle>
-      <Dropdown.Menu >
-        { cities.map(city => (
-           <Dropdown.Item key={city.id} eventKey={city.name}>{city.name}</Dropdown.Item>
-          ))
-        }
-      </Dropdown.Menu>
-    </Dropdown>
-  );
-}
-
-const TripleButtonPlaceholder = ({}) => {
-  return (
-    <ButtonGroup style={{ width: '100%'}}>
-      <Placeholder.Button aria-hidden="true" variant="secondary" style={{ width: '33%'}}/>
-      <Placeholder.Button aria-hidden="true" variant="secondary" style={{ width: '33%'}}/>
-      <Placeholder.Button aria-hidden="true" variant="secondary" style={{ width: '33%'}}/>
-    </ButtonGroup>
-  )
-};
-
-const TripleButtonGroup = ({ radioValue, setRadioFunction, radioLabels}) => {
-  return (
-    <ButtonGroup style={{ width: '100%'}}>
-      {radioLabels.map((radio) => (
-        <ToggleButton
-          key={radio.id}
-          id={radio.id}
-          type="radio"
-          variant={radioValue == radio.id ? ( 'outline-primary' ) : ('outline-secondary')}
-          name="radio"
-          value={radio.id}
-          checked={radioValue == radio.id}
-          onChange={(e) => setRadioFunction(e.target.value)}
-        >
-          {radio.seniority} {radio.role}
-        </ToggleButton>
-      ))}
-    </ButtonGroup>
-
-  );
-}
-
-const SalaryTable = ({salariesGiven, radioValue, taxGiven}) => {
-  if (salariesGiven.data.length === 0 || radioValue === 0 || !taxGiven){
-    return <EmptyTable/>;
-  } 
-  let currency = salariesGiven.data[0]["currency"];
-  let salary = salariesGiven.data.find(salary => salary.id === radioValue)
-  if ( currency === '€'){
-    return (<TwoRowTable salaryChosen={salariesGiven.data[radioValue-1]} tax={taxGiven}></TwoRowTable>);
-  } else {
-    return(<FourRowTable salaryChosen={salariesGiven.data[radioValue-1]} tax={taxGiven}></FourRowTable>);
-  }
-}
-
-const FourRowTable = ({salaryChosen, tax}) => {
-  return (
-    <div>
-      <Table bordered striped size="sm" variant="dark">
-        <tbody>
-          <tr>
-            <td className="align-middle" rowSpan={2}>
-              Salary
-            </td>
-            <td className="annual-numbers"> 
-              { salaryChosen.currency } { salaryChosen.compensation.toLocaleString("en-US") }
-            </td>
-            <td className="align-middle" rowSpan={2}>
-              Tax
-            </td>
-            <td className="annual-numbers">
-            { salaryChosen.currency } { tax.data.taxPayable && tax.data.taxPayable.toLocaleString("en-US") } 
-            </td>
-          </tr>
-          <tr>
-            <td className="annual-numbers">
-              € { salaryChosen.compensationConverted.toLocaleString("en-US") }
-            </td>
-            <td className="annual-numbers">
-          
-            € { tax.data.taxPayableConverted && tax.data.taxPayableConverted.toLocaleString("en-US") }
-            </td>
-          </tr>
-        </tbody>
-      </Table>
-    </div>
-  );
-}
-
-const TwoRowTable = ({salaryChosen, tax}) => {
-  return (
-    <div>
-      <Table bordered striped size="sm" variant="dark">
-        <tbody>
-          <tr>
-            <td className="align-middle">
-              Salary
-            </td>
-            <td className="align-right">
-              € { salaryChosen.compensationConverted.toLocaleString("en-US") }
-            </td>
-            <td className="align-middle">
-              Tax
-            </td>
-            <td className="align-right">
-              € { tax.data.taxPayableConverted && tax.data.taxPayable.toLocaleString("en-US") }
-            </td>
-          </tr>
-        </tbody>
-      </Table>
-    </div>
-  );
-}
-
-const EmptyTable = () => {
-  return (
-    <div>
-      <Table bordered striped size="sm" variant="dark">
-        <tbody>
-          <tr>
-            <td className="align-middle">
-              Salary
-            </td>
-            <td>
-              € Salary
-            </td>
-            <td className="align-middle">
-              Tax
-            </td>
-            <td>
-              € Tax
-            </td>
-          </tr>
-        </tbody>
-      </Table>
-    </div>
-  );
-}
 
 
 
